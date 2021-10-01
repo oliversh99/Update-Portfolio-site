13c6ed97b8af1229a11e3076a37960978818ee11
oliversh99 <oliv.code999@gmail.com>
2021-09-30 23:45:29 -0700

    first send

---
+++ script.js
@@ -0,0 +1,17 @@
+var carousel= document.querySelector('.carousel');
+var myWorkBtn = document.querySelector('#myWorkBtn');
+myWorkBtn.addEventListener('click',function(){
+carousel.classList.remove('heddin')
+});
+
+
+
+
+var showAboutme= document.querySelector('.about');
+var aboutMeBtn = document.querySelector('#aboutMeBtn');
+aboutMeBtn.addEventListener('click',function(){
+  showAboutme.classList.remove('hide')
+
+});
+
+
